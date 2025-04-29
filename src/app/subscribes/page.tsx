3b63bde40f70e1c8e3f88c8e8e020812ec8a80e1
Vedmid20 <lysyy.boba.freelance@gmail.com>
2025-04-29 14:46:53 +0300

    Styling subscribes page

--- src/app/subscribes/page.tsx
+++ src/app/subscribes/page.tsx
@@ -3,7 +3,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
-import { Briefcase, Layers, ClipboardList, Hash, CalendarDays } from 'lucide-react';
+import { Briefcase, Layers, ClipboardList, Hash, CalendarDays, Bug, Check, CheckCircle, Star, MoreHorizontal, MoreVertical } from 'lucide-react';
 
 interface TaskSubscriber {
   id: number;
@@ -66,30 +66,34 @@ const SubscribesPage = () => {
       <h1 className="mb-4">Subscribes</h1>
       <title>Subscribes</title>
       {subscriptions.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 gap-4">
           {subscriptions.map((subscription) => (
-            <div key={subscription.id} className="p-4 dark:bg-black/5 rounded-lg relative shadow flex items-center border-t-8 border border-violet-500">
+            <div key={subscription.id} className="p-2 dark:bg-black/5 rounded-lg relative shadow flex border-t-8 border border-violet-500">
               <div className="w-full">
-                <div className="flex gap-4 items-center">
-                  <Layers className="w-16 h-16 p-2 rounded-full bg-violet-500 text-white" />
-                  <p className="text-[1.5rem]">{subscription.task_title || `Task #${subscription.task}`}</p>
+                <div className="flex my-auto justify-between">
+                  <div className="flex gap-1 items-center">
+                    <Hash className="w-16 h-16 p-2 rounded-full " />
+                    <p className="text-[1.5rem]">{subscription.task_details.project_task_id}</p>
+                  </div>
+                  <div className="flex my-auto">
+                    <Star className="w-10 h-10 p-2 rounded-full hover:text-yellow-400 dark:hover:text-yellow-300 transition cursor-pointer" strokeWidth={1}/>
+                    <MoreVertical className="w-10 h-10 p-2 rounded-full transition cursor-pointer hover:bg-black/5 dark:hover:bg-white/10"/>
+                  </div>
                 </div>
-                <div className="bg-white/0 w-auto rounded-lg p-3 mt-3">
-                  <p className="text-sm border-b-2 pb-2 border-black/50 dark:border-white/75 flex">
-                    <Hash className='w-6 h-6 text-black/75 dark:text-white my-1 mr-2' />
-                    <span className='my-auto'>Task ID: {subscription.task}</span>
+                <div className="bg-transparent w-auto rounded-lg p-3">
+                  <p className="flex">
+                    <Bug className='w-6 h-6 text-black/75 dark:text-white my-1 mr-2' />
+                    <span className='bg-violet-500/50 px-3 rounded-full text-black dark:text-white my-auto'>{subscription.task_details.type_name}</span>
                   </p>
-                  <p className="text-sm flex">
-                    <Briefcase className="w-6 h-6 text-black/75 dark:text-white my-1 mr-2" />
-                    <span className='my-auto'>Status: {subscription.task_status || 'Unknown'}</span>
+                  <p className="flex">
+                    <CheckCircle className="w-6 h-6 text-black/75 dark:text-white my-1 mr-2" />
+                    <span className='bg-black/10 dark:bg-white/10 px-3 rounded-full text-black dark:text-white my-auto'>{subscription.task_details.status_name}</span>
                   </p>
-                  <p className="text-sm flex">
-                    <ClipboardList className="w-6 h-6 text-black/75 dark:text-white my-1 mr-2" />
-                    <span className='my-auto'>Subscription: {subscription.subscription_type}</span>
+                  <p className="text-sm py-2 my-2 bg-black/5 dark:bg-white/10 p-2 rounded-lg border-t-8 border-t-black/10 dark:border-t-white/15">
+                    <span className='my-auto'>{subscription.task_details.title}</span>
                   </p>
-                  <p className="text-sm flex">
-                    <CalendarDays className="w-6 h-6 text-black/75 dark:text-white my-1 mr-2" />
-                    <span className='my-auto'>Subscribed on: {new Date(subscription.created_at).toLocaleDateString()}</span>
+                  <p className="text-sm">
+                    <span className='my-auto'>Deadline {new Date(subscription.task_details.deadline).toLocaleDateString()}</span>
                   </p>
                 </div>
               </div>
